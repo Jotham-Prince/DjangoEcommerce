8de7f1b9cc69d7a71812dbec044376a28b9283b7
Jotham-Prince <jothamprince34@gmail.com>
2024-05-10 23:02:05 +0300

    static dir bug fix

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -131,11 +131,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 print("MEDIA_ROOT::", MEDIA_ROOT)
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#   )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+   )
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
